c88ecd457c0eb0ae548afd09a6f95374937cf876
Eman <youngamer_@outlook.com>
2020-10-14 10:44:35 +0200

    service app without client’s feedback

--- service/views.py
+++ service/views.py
@@ -6,3 +6,10 @@ from django.views.generic import ListView
 
 class ServiceList(ListView):
     model= Service
+   # extra_context=Facts.objects.last() # listview attribute to get another data  from another model in same view #  last() cuze its one DB wich doesnt need creating a new one just edit on it
+    #or
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["x"] = Facts.objects.last() #query
+        return context
+    
